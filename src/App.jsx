a1951b5bacb04a934c354163c625cbb3a0ed509a
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-02-18 17:50:32 +0100

    azzero il valore iniziale del form dopo il submit

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,23 @@ import { useState } from 'react'
 function App() {
 
   // Creo l'array di articoli
-  const articoles = ["Le meraviglie del cosmo: esplorando l'universo", "Cucina italiana: ricette tradizionali rivisitate", "Tecnologia verde: il futuro sostenibile", "Arte contemporanea: tendenze e protagonisti", "Viaggi low-cost: destinazioni imperdibili"];
+  const articles = ["Le meraviglie del cosmo: esplorando l'universo", "Cucina italiana: ricette tradizionali rivisitate", "Tecnologia verde: il futuro sostenibile", "Arte contemporanea: tendenze e protagonisti", "Viaggi low-cost: destinazioni imperdibili"];
 
-  const [newArticle, setNewArticle] = useState("Innovazione digitale: esempi e trend del futuro");
+  const [article, setArticle] = useState(articles);
+  const [newArticle, setNewArticle] = useState("");
 
   const handleSubmit = event => {
     event.preventDefault();
-    console.log("Il nuovo articolo s'intitola: " + newArticle)
+    setNewArticle([...article, newArticle]);
+    // azzero il valore di newArticle
+    setNewArticle('');
   }
 
   return (
     <>
       <div className="container">
         <ul>
-          {articoles.map((article, index) => (
+          {articles.map((article, index) => (
             <li
               key={index}>{article}</li>
           ))}
@@ -25,7 +28,7 @@ function App() {
       <div className="container">
         <form onSubmit={handleSubmit} action="/action_page.php" method="get">
           <label>Aggiungi Titolo:</label>
-          <input type="text" id="fname" name="fname" value={newArticle} onChange={e => { setNewArticle(e.target.value) }} placeholder="Inserire il nuovo titolo qui"></input>
+          <input type="text" id="fname" name="fname" value={newArticle} onChange={event => { setNewArticle(event.target.value) }} placeholder="Inserire il nuovo titolo qui"></input>
           <input type="submit" value="Aggiungi"></input>
         </form>
       </div>
