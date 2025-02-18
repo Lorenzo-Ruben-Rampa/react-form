5c6ea097b38d159656885f8d9f4114a3b265ef30
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-02-18 18:57:20 +0100

    impostazione bonus

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,47 @@ function App() {
   // Creo l'array di articoli
   const initialArticles = ["Le meraviglie del cosmo: esplorando l'universo", "Cucina italiana: ricette tradizionali rivisitate", "Tecnologia verde: il futuro sostenibile", "Arte contemporanea: tendenze e protagonisti", "Viaggi low-cost: destinazioni imperdibili"];
 
+  // BONUS
+  // const initialArticlesArray = [
+  //   {
+  //     titolo: "Le meraviglie del cosmo: esplorando l'universo",
+  //     occhiello: "Un viaggio attraverso le galassie e i misteri dello spazio profondo.",
+  //     categoria: "Scienza"
+  //   },
+  //   {
+  //     titolo: "Cucina italiana: ricette tradizionali rivisitate",
+  //     occhiello: "Scopri come innovare i piatti classici della tradizione culinaria italiana.",
+  //     categoria: "Cucina"
+  //   },
+  //   {
+  //     titolo: "Tecnologia verde: il futuro sostenibile",
+  //     occhiello: "Come le innovazioni tecnologiche stanno contribuendo a un pianeta più pulito.",
+  //     categoria: "Ambiente"
+  //   },
+  //   {
+  //     titolo: "Arte contemporanea: tendenze e protagonisti",
+  //     occhiello: "Un'analisi delle correnti artistiche moderne e dei loro principali esponenti.",
+  //     categoria: "Arte"
+  //   },
+  //   {
+  //     titolo: "Viaggi low-cost: destinazioni imperdibili",
+  //     occhiello: "Consigli e trucchi per viaggiare il mondo senza spendere una fortuna.",
+  //     categoria: "Viaggi"
+  //   },
+  //   {
+  //     titolo: "Innovazione digitale: esempi e trend del futuro",
+  //     occhiello: "Esploriamo come la trasformazione digitale sta cambiando le nostre vite e quali sono le prospettive future.",
+  //     categoria: "Tecnologia"
+  //   }
+  // ];
+
+  //  Nuovo articolo da aggiungere
+  // {
+  //   titolo: "Innovazione digitale: esempi e trend del futuro",
+  //   occhiello: "Esploriamo come la trasformazione digitale sta cambiando le nostre vite e quali sono le prospettive future.",
+  //   categoria: "Tecnologia"
+  // }
+
   const [articles, setArticles] = useState(initialArticles);
   const [newArticle, setNewArticle] = useState("");
 
